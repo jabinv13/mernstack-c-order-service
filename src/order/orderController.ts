66872df9254927fdabb24d2c06bab8d2e85c4805
jabin-v <jabinv03@gmail.com>
2024-10-07 17:57:54 +0530

    Calculate Discount Amount

--- src/order/orderController.ts
+++ src/order/orderController.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
 import { OrderService } from "./orderService";
 import { Logger } from "winston";
+import {
+  CartItem,
+  ProductPricingCache,
+  Topping,
+  ToppingPriceCache,
+} from "../types";
+import productCacheModel from "../productCache/productCacheModel";
+import toppingCacheModel from "../toppingCache/toppingCacheModel";
+import couponModel from "../coupon/couponModel";
 export class OrderController {
   constructor(
     private orderService: OrderService,
@@ -8,6 +17,139 @@ export class OrderController {
   ) {}
 
   async create(req: Request, res: Response) {
-    return res.json({ id: "nskks" });
+    const {
+      cart,
+      couponCode,
+      tenantId,
+      paymentMode,
+      customerId,
+      comment,
+      address,
+    } = req.body;
+    const totalPrice = await this.calculateTotal(cart);
+    let discountPercentage = 0;
+    if (couponCode) {
+      discountPercentage = await this.getDiscountPercentage(
+        couponCode,
+        tenantId,
+      );
+    }
+
+    const discountAmount = Math.round((totalPrice * discountPercentage) / 100);
+
+    const priceAfterDiscount = totalPrice - discountAmount;
+
+    // todo: May be store in db for each tenant.
+    const TAXES_PERCENT = 18;
+
+    const taxes = Math.round((priceAfterDiscount * TAXES_PERCENT) / 100);
+    // todo: may be store in database for each tenant or maybe calculated according to business rules.
+    const DELIVERY_CHARGES = 100;
+
+    const finalTotal = priceAfterDiscount + taxes + DELIVERY_CHARGES;
+
+    res.json({});
   }
+
+  private calculateTotal = async (cart: CartItem[]) => {
+    const productIds = cart.map((item) => item._id);
+
+    const productPricings = await productCacheModel.find({
+      productId: {
+        $in: productIds,
+      },
+    });
+
+    // todo: What will happen if product does not exists in the cache
+    // 1. call catalog service.
+    // 2. Use price from cart <- BAD
+
+    const cartToppingIds = cart.reduce((acc, item) => {
+      return [
+        ...acc,
+        ...item.chosenConfiguration.selectedToppings.map(
+          (topping) => topping.id,
+        ),
+      ];
+    }, []);
+
+    // todo: What will happen if topping does not exists in the cache
+    const toppingPricings = await toppingCacheModel.find({
+      toppingId: {
+        $in: cartToppingIds,
+      },
+    });
+
+    const totalPrice = cart.reduce((acc, curr) => {
+      const cachedProductPrice = productPricings.find(
+        (product) => product.productId === curr._id,
+      );
+
+      return (
+        acc +
+        curr.qty * this.getItemTotal(curr, cachedProductPrice, toppingPricings)
+      );
+    }, 0);
+
+    return totalPrice;
+  };
+
+  private getItemTotal = (
+    item: CartItem,
+    cachedProductPrice: ProductPricingCache,
+    toppingsPricings: ToppingPriceCache[],
+  ) => {
+    const toppingsTotal = item.chosenConfiguration.selectedToppings.reduce(
+      (acc, curr) => {
+        return acc + this.getCurrentToppingPrice(curr, toppingsPricings);
+      },
+      0,
+    );
+
+    const productTotal = Object.entries(
+      item.chosenConfiguration.priceConfiguration,
+    ).reduce((acc, [key, value]) => {
+      const price =
+        cachedProductPrice.priceConfiguration[key].availableOptions[value];
+      return acc + price;
+    }, 0);
+
+    return productTotal + toppingsTotal;
+  };
+
+  private getCurrentToppingPrice = (
+    topping: Topping,
+    toppingPricings: ToppingPriceCache[],
+  ) => {
+    const currentTopping = toppingPricings.find(
+      (current) => topping.id === current.toppingId,
+    );
+
+    if (!currentTopping) {
+      // todo: Make sure the item is in the cache else, maybe call catalog service.
+      return topping.price;
+    }
+
+    return currentTopping.price;
+  };
+
+  private getDiscountPercentage = async (
+    couponCode: string,
+    tenantId: string,
+  ) => {
+    const code = await couponModel.findOne({ code: couponCode, tenantId });
+
+    if (!code) {
+      return 0;
+    }
+
+    const currentDate = new Date();
+    const couponDate = new Date(code.validUpto);
+
+    if (currentDate <= couponDate) {
+      return code.discount;
+    }
+
+    return 0;
+  };
 }
