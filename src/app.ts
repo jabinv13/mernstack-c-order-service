4f3df20e79be9d8949255558568113b9d1188396
jabin-v <jabinv03@gmail.com>
2024-10-07 11:34:28 +0530

    Implement Kafka Consumer

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,26 @@ import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import customerRouter from "./customer/customerRouter";
 import couponRouter from "./coupon/couponRouter";
+import cors from "cors";
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(
+  cors({
+    //todo:move to .env file
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from order service service!" });
 });
 
 app.use("/customer", customerRouter);
-app.use("/coupon", couponRouter);
+app.use("/coupons", couponRouter);
 
 app.use(globalErrorHandler);
 
