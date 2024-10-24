2cc0f15f98f6f9a129b37eb1512c96f6a968c7d9
jabin-v <jabinv03@gmail.com>
2024-10-24 15:16:35 +0530

    kafka message structure correction

--- src/productCache/productUpdateHandler.ts
+++ src/productCache/productUpdateHandler.ts
@@ -3,15 +3,16 @@ import productCacheModel from "./productCacheModel";
 
 export const handleProductUpdate = async (value: string) => {
   // todo: wrap this parsing in try catch
+  //todo:utilize event
   const product: ProductMessage = JSON.parse(value);
 
   return await productCacheModel.updateOne(
     {
-      productId: product.id,
+      productId: product.data.id,
     },
     {
       $set: {
-        priceConfiguration: product.priceConfiguration,
+        priceConfiguration: product.data.priceConfiguration,
       },
     },
     { upsert: true },
